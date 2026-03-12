49f36be26e5d1be492b663b446061be90b85d2aa
shadow-Sx <shadowsoya8@gmail.com>
2026-03-12 05:33:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -290,7 +290,7 @@ def start(message):
 
     channel_btn = types.InlineKeyboardMarkup()
     channel_btn.add(
-        types.InlineKeyboardButton("📢 Kanalga o'tish", url=f"https://t.me/c/{str(CHANNEL_ID)[4:]}")
+        types.InlineKeyboardButton("📢 Kanalga o'tish", url=f"https://t.me/c/{str(CHANNEL_ID)[4:]}/119")
     )
 
     if not is_subscribed(user_id):
